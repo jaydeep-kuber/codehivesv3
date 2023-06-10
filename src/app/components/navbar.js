063abfb52a9a50c36a6c6a38f210ac0933f38351
jaydeep-kuber <jayofficial085@gmail.coom>
2023-06-10 18:24:42 +0530

    clickMe_rmv

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -62,11 +62,11 @@ function Navbar() {
            Bee Hive 
          </a>
        </div>
-       <div>
+       {/* <div>
          <button className="inline-flex items-center bg-amber-500 border-0 py-2 px-4 text-white">
            Click Me
          </button>
-       </div>
+       </div> */}
      </div>
    </nav>
  );
